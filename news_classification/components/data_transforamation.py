c923b5f59d07b4a421532c2ea4d9b26e20376160
Atiq Mansoori <er_asm@outlook.com>
2024-06-01 10:10:19 +0530

    Data Transformation

--- news_classification/components/data_transforamation.py
+++ news_classification/components/data_transforamation.py
@@ -81,6 +81,7 @@ class DataTransformation:
 
             label = LabelEncoder()
             tfidf = TfidfVectorizer()
+            
             # Label Encoding for target variables
 
             #training dataframe
